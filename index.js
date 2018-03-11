d0959dc30c51ecb8a530c82ec94abf3cf24a3470
RamenNoodo <37251708+RamenNoodo@users.noreply.github.com>
2018-03-11 11:33:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bot.on('message', (message) => {
 
     if(message.content == 'ping') {
         //message.reply('pong');
-        message.channel.deleteMessage('pang')
+        message.channel.deleteMessage('ping')
     }
 
 });
